96d0142ad50e088079764d3e54decc7ee0e355a6
Minh Nhat Le <nhatminh.vnm@gmail.com>
2025-03-24 16:34:10 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/status", (request, response) => {
+   const status = {
+      "Status": "Running"
+   };
+
+   response.send(status);
+});
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
